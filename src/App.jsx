e4e458c494af7f7629bc4423ba3df68d251c2f8f
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-12-06 23:53:13 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Hero from "./sections/Hero/Hero"
 
 const App = () => {
   return (
-    <div>Hero </div>
+    <div><Hero/></div>
+    
   )
+  
 }
 
 export default App
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>Hii i am shubham </div>
   )
 }
 
